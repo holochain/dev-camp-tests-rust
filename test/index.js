89a05fd90145ce206c76a5dc57daef1b9eba1985
Connor Turland <connorturland@gmail.com>
2018-11-20 17:02:24 -0500

    add placeholder tests

--- test/index.js
+++ test/index.js
@@ -9,13 +9,13 @@ const app = Container.loadAndInstantiate("dist/bundle.json")
 // activate the new instance
 app.start()
 
-test('use the commit function to add a person entry', (t) => {
+test('use the commit_entry function to add a person entry', (t) => {
   const result = app.call("people", "main", "add_person", JSON.stringify({ name: "Bonnitta" }))
   t.equal(JSON.parse(result).address, "QmWyA4MpWazSQBEh7WLTLdHPFCUk31hbcacnJr87LCWR9T")
   t.end()
 })
 
-// test('use the get function to get a person entry', (t) => {
+// test('use the get_entry function to get a person entry', (t) => {
 //   const result = app.call("people", "main", "add_person", JSON.stringify({ name: "Bonnitta" }))
 //   t.equal(JSON.parse(result), "QmaErP4TAA7C39JfDFqzq3R57GnhevtoVC7H5eWK9r8hWU")
 //   t.end()
@@ -27,7 +27,13 @@ test('use validation rules to ensure that a persons name is equal to or greater 
   t.end()
 })
 
-// test('use the commit function to add a person entry', (t) => {
+// test('use the link_entries function to link two people entries', (t) => {
+//   const result = app.call("people", "main", "add_person", JSON.stringify({ name: "Bonnitta" }))
+//   t.equal(JSON.parse(result), "QmaErP4TAA7C39JfDFqzq3R57GnhevtoVC7H5eWK9r8hWU")
+//   t.end()
+// })
+
+// test('use the get_links function to return people linked from Bonnitta', (t) => {
 //   const result = app.call("people", "main", "add_person", JSON.stringify({ name: "Bonnitta" }))
 //   t.equal(JSON.parse(result), "QmaErP4TAA7C39JfDFqzq3R57GnhevtoVC7H5eWK9r8hWU")
 //   t.end()
