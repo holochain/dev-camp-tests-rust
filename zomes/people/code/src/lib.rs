d9132f69d04c8421980302f42bb28fd96143b7be
Connor Turland <connorturland@gmail.com>
2018-11-29 13:52:44 -0500

    improve tests and cleanup lib.rs

--- zomes/people/code/src/lib.rs
+++ zomes/people/code/src/lib.rs
@@ -20,23 +20,9 @@ use hdk::{
         entry::Entry,
         entry::entry_type::EntryType,
         error::HolochainError,
+        cas::content::Address,
     },
 };
-
-#[derive(Serialize, Deserialize, Debug, DefaultJson)]
-struct Person {
-    name: String,
-}
-
-fn handle_add_person(name: String) -> JsonString {
-    let person_entry = Entry::new(EntryType::App("person".into()), Person {
-        name,
-    });
-    match hdk::commit_entry(&person_entry) {
-        Ok(address) => json!({ "address": address }).into(),
-        Err(hdk_error) => hdk_error.into(),
-    }
-}
 
 define_zome! {
     entries: [
