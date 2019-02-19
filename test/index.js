624f4206c119e32802b2d8f4c579538786460016
Connor Turland <connorturland@gmail.com>
2019-02-19 12:13:39 -0500

    prepare tests

--- test/index.js
+++ test/index.js
@@ -1,70 +1,86 @@
+
 // This test file uses the tape testing framework.
 // To learn more, go here: https://github.com/substack/tape
-const test = require('tape-catch');
-const { Config, Conductor } = require("../../../rust/nodejs_conductor");
+const { Config, Scenario } = require("@holochain/holochain-nodejs")
+Scenario.setTape(require('tape-catch'))
 
-// instantiate an alice from the DNA JSON bundle
-const aliceName = "alice"
-const dnaPath = "dist/bundle.json"
-const instanceAlice = Config.instance(Config.agent(aliceName), Config.dna(dnaPath))
-const config = Config.conductor([instanceAlice], { debugLog: false })
-const conductor = new Conductor(config)
-conductor.start()
+const dnaPath = "./dist/bundle.json"
+const agentAlice = Config.agent("alice")
+const dna = Config.dna(dnaPath)
+const instanceAlice = Config.instance(agentAlice, dna)
+const scenario = new Scenario([instanceAlice])
 
 const bonnittaAddress = "QmbL7tDsQumvsUTDVZo5mtJknhV6bT28yZDuTdyHQdfqTs"
 
-test('use the commit_entry function to add a person entry', (t) => {
+scenario.runTape("use the commit_entry function to add a person entry", (t, { alice }) => {
   let result
   try {
-    result = conductor.call(aliceName, "people", "add_person", { name: "Bonnitta" })
+    result = alice.call("people", "add_person", { name: "Bonnitta" })
   } catch (e) {}
   t.deepEqual(result, { Ok: bonnittaAddress })
-  t.end()
 })
 
-test('use the get_entry function to get a person entry', (t) => {
+scenario.runTape("use the get_entry function to get a person entry", (t, { alice }) => {
   let result
   try {
-    result = conductor.call(aliceName, "people", "get_person", { address: bonnittaAddress })
+    alice.call("people", "add_person", { name: "Bonnitta" })
+    result = alice.call("people", "get_person", { address: bonnittaAddress })
   } catch (e) {}
   t.deepEqual(result, { Ok: { App: [ 'person', '{"name":"Bonnitta"}' ] } })
-  t.end()
 })
 
-test('use validation rules to ensure that a persons name is equal to or greater than 2 characters', (t) => {
+scenario.runTape("use the get_entry function to get a person entry", (t, { alice }) => {
+  let result
+  try {
+    alice.call("people", "add_person", { name: "Bonnitta" })
+    result = alice.call("people", "get_person", { address: bonnittaAddress })
+  } catch (e) {}
+  t.deepEqual(result, { Ok: { App: [ 'person', '{"name":"Bonnitta"}' ] } })
+})
+
+scenario.runTape("use validation rules to ensure that a persons name is equal to or greater than 2 characters", (t, { alice }) => {
   let result = {}
   try {
-    result = conductor.call(aliceName, "people", "add_person", { name: "B" })
+    result = alice.call("people", "add_person", { name: "B" })
   } catch (e) {}
   t.notEqual(result.Err, undefined)
-  t.end()
 })
 
-test('use the link_entries function to link two people entries', async (t) => {
+scenario.runTape("use the link_entries function to link two people entries", async (t, { alice }) => {
   let addResult, result
   try {
-    addResult = await conductor.callSync(aliceName, "people", "add_person", {
+    addResult = await alice.callSync("people", "add_person", {
       name: "Vincenzo"
     })
-    result = await conductor.callSync(aliceName, "people", "link_people", {
+    result = await alice.callSync("people", "link_people", {
       base: bonnittaAddress,
       target: addResult.Ok,
       tag: "is friends with"
     })
   } catch (e) {}
   t.deepEqual(result, { Ok: null })
-  t.end()
 })
 
-test('use the get_links function to return people linked from Bonnitta', (t) => {
+scenario.runTape("use the get_links function to return people linked from Bonnitta", async (t, { alice }) => {
   let result
   try {
-    result = conductor.call(aliceName, "people", "get_relationships", {
+    // add Bonnitta
+    await alice.callSync("people", "add_person", { name: "Bonnitta" })
+    // add Vincenzo
+    let addResult = await alice.callSync("people", "add_person", {
+      name: "Vincenzo"
+    })
+    // make Bonnitta friends with Vincenzo
+    await alice.callSync("people", "link_people", {
+      base: bonnittaAddress,
+      target: addResult.Ok,
+      tag: "is friends with"
+    })
+    // get a list of the address of people who are friends with Bonnitta
+    result = alice.call("people", "get_relationships", {
       address: bonnittaAddress,
       tag: "is friends with"
     })
   } catch (e) {}
   t.deepEqual(result, { Ok: { addresses: [ "QmPcNictUVyk9tki1TwnsZ2RzzuPYdNPoFXZReRQLUJb4X" ] } })
-  t.end()
-  conductor.stop()
 })
--- zomes/people/code/src/lib.rs
+++ zomes/people/code/src/lib.rs
@@ -28,5 +28,5 @@ define_zome! {
 
     functions: []
 
-    capabilities: {}
+    traits: {}
 }
